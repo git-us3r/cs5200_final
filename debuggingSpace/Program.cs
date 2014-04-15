d6e7ea2c36894c6fd1cb174716ac7f7b4049c0e2
git-us3r <mlink3.14@gmail.com>
2014-04-14 21:38:48 -0600

    first commit!

---
+++ debuggingSpace/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Common;
+
+
+namespace debuggingSpace
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Registration.RegistrarClient rc = new Registration.RegistrarClient();
+           
+            var response = rc.GetGames(GameInfo.GameStatus.AVAILABLE);
+
+
+            Console.WriteLine("");
+
+            Console.ReadKey();
+        }
+    }
+}
